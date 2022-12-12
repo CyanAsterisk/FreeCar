087c8211f2e2eb1bbe303d305a256bad3eec4410
Claude-Zq <1574089391@qq.com>
2022-12-12 20:18:43 +0800

    feat: complete resolve

--- miniprogram/service/request.ts
+++ miniprogram/service/request.ts
@@ -2,7 +2,7 @@ import camelcaseKeys = require("camelcase-keys")
 import { auth } from "./proto_gen/auth/auth_pb"
 
 export namespace FreeCar {
-    export const serverAddr = 'http://localhost:8080'
+    export const serverAddr = 'http://localhost:9900'
     export const wsAddr = 'ws://localhost:9090'
     const AUTH_ERR = 'AUTH_ERR'
 
--- server/cmd/auth/tool/wechat.go
+++ server/cmd/auth/tool/wechat.go
@@ -1,8 +1,25 @@
 package tool
 
-// Resolve resolves authorization code to WeChat open id.
-// TODO By @Claud_Zq
-func Resolve(code string) string {
-	resp := "openid" + code
-	return resp
+import (
+	"github.com/CyanAsterisk/FreeCar/server/cmd/auth/global"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/medivhzhan/weapp/v2"
+)
+
+type AuthServiceImpl struct {
+}
+
+// Resolve resolves authorization code to WeChat open id,return empty string if an Error occurs.
+func (s *AuthServiceImpl) Resolve(code string) string {
+	resp, err := weapp.Login(global.ServerConfig.WXInfo.AppId, global.ServerConfig.WXInfo.AppSecret, code)
+
+	if err != nil {
+		klog.Errorf("WeApp.Login Err: %v code:%s", err, code)
+		return ""
+	}
+	if err := resp.GetResponseError(); err != nil {
+		klog.Errorf("WeApp.Login resp Err: %v code:%s", err, code)
+		return ""
+	}
+	return resp.OpenID
 }

4ddec0b6fa52f4fb065be4df07a9120447ba7a78
清 <claude-zq@qq.com>
2022-12-20 17:16:12 +0800

    refactor: Refactored the dao layer

--- server/cmd/car/global/global.go
+++ server/cmd/car/global/global.go
@@ -2,14 +2,14 @@ package global
 
 import (
 	"github.com/CyanAsterisk/FreeCar/server/cmd/car/config"
-	"github.com/CyanAsterisk/FreeCar/server/cmd/car/dao"
 	"github.com/CyanAsterisk/FreeCar/server/cmd/car/kitex_gen/car/carservice"
 	"github.com/CyanAsterisk/FreeCar/server/cmd/car/tool/mq"
 	"github.com/CyanAsterisk/FreeCar/server/cmd/trip/kitex_gen/trip/tripservice"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
-	DB           *dao.Mongo
+	Col          *mongo.Collection
 	ServerConfig config.ServerConfig
 	NacosConfig  config.NacosConfig
 	Publisher    mq.Publisher

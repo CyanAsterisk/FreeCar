e6efc048b215f266a3c431bb0f5a560b0c472926
L2ncE <llance_24@foxmail.com>
2022-12-27 20:44:58 +0800

    feat: add recover and jwt mw

--- server/cmd/api/biz/errno/errno.go
+++ server/cmd/api/biz/errno/errno.go
@@ -23,6 +23,7 @@ func (e ErrNo) Error() string {
 	return fmt.Sprintf("err_code=%d, err_msg=%s", e.ErrCode, e.ErrMsg)
 }
 
+// NewErrNo return ErrNo
 func NewErrNo(code int64, msg string) ErrNo {
 	return ErrNo{
 		ErrCode: code,

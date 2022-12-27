540c23f2ba906e6479a39e73d1a95267c84d9aa0
L2ncE <llance_24@foxmail.com>
2022-12-27 20:27:25 +0800

    Revert "style: improve mw log"
    This reverts commit 6450c23977810ad79acc9e891ae9f5381088543d.

--- shared/middleware/client.go
+++ shared/middleware/client.go
@@ -15,7 +15,7 @@ func ClientMiddleware(next endpoint.Endpoint) endpoint.Endpoint {
 	return func(ctx context.Context, req, resp interface{}) (err error) {
 		ri := rpcinfo.GetRPCInfo(ctx)
 		// get server information
-		klog.Infof("[KITEX] server address: %v, rpc timeout: %v, readwrite timeout: %v\n", ri.To().Address(), ri.Config().RPCTimeout(), ri.Config().ConnectTimeout())
+		klog.Infof("server address: %v, rpc timeout: %v, readwrite timeout: %v\n", ri.To().Address(), ri.Config().RPCTimeout(), ri.Config().ConnectTimeout())
 		if err = next(ctx, req, resp); err != nil {
 			return err
 		}

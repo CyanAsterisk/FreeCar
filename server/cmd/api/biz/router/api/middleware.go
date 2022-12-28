493c652ade54a03ed6586b84c5d326ee006a0fd5
L2ncE <llance_24@foxmail.com>
2022-12-28 19:34:48 +0800

    feat: add recovery mw and refactor mw

--- server/cmd/api/biz/router/api/middleware.go
+++ server/cmd/api/biz/router/api/middleware.go
@@ -3,16 +3,8 @@
 package Api
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/CyanAsterisk/FreeCar/server/cmd/api/biz/errno"
 	"github.com/CyanAsterisk/FreeCar/shared/middleware"
 	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
-	"github.com/cloudwego/hertz/pkg/common/hlog"
-	"github.com/cloudwego/hertz/pkg/common/utils"
-	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
 
 func rootMw() []app.HandlerFunc {
@@ -93,16 +85,7 @@ func _loginMw() []app.HandlerFunc {
 
 func _profileMw() []app.HandlerFunc {
 	return []app.HandlerFunc{
-		// use recovery mw
-		recovery.Recovery(recovery.WithRecoveryHandler(
-			func(ctx context.Context, c *app.RequestContext, err interface{}, stack []byte) {
-				hlog.SystemLogger().CtxErrorf(ctx, "[Recovery] err=%v\nstack=%s", err, stack)
-				c.JSON(consts.StatusInternalServerError, utils.H{
-					"code":    errno.BadRequest,
-					"message": fmt.Sprintf("[Recovery] err=%v\nstack=%s", err, stack),
-				})
-			},
-		)),
+		middleware.Recovery(),
 		middleware.JWTAuth(),
 	}
 }
@@ -119,16 +102,7 @@ func _clearprofilephotoMw() []app.HandlerFunc {
 
 func _photoMw() []app.HandlerFunc {
 	return []app.HandlerFunc{
-		// use recovery mw
-		recovery.Recovery(recovery.WithRecoveryHandler(
-			func(ctx context.Context, c *app.RequestContext, err interface{}, stack []byte) {
-				hlog.SystemLogger().CtxErrorf(ctx, "[Recovery] err=%v\nstack=%s", err, stack)
-				c.JSON(consts.StatusInternalServerError, utils.H{
-					"code":    errno.BadRequest,
-					"message": fmt.Sprintf("[Recovery] err=%v\nstack=%s", err, stack),
-				})
-			},
-		)),
+		middleware.Recovery(),
 		middleware.JWTAuth(),
 	}
 }

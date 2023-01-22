df945c66f888b9fe0cd2fd015e0f495f81eb446e
清 <claude-zq@qq.com>
2023-01-22 19:46:13 +0800

    fix: remove wxapi

--- static/pages/user/index.ts
+++ static/pages/user/index.ts
@@ -1,4 +1,3 @@
-import WXAPI = require('apifm-wxapi/index')
 import { ProfileService } from "../../service/profile"
 import { api } from "../../service/codegen/api_pb"
 
@@ -28,30 +27,5 @@ Page({
     })
   },
   async onChooseAvatar(e: { detail: { avatarUrl: any; }; }) {
-    console.log(e);
-    const avatarUrl = e.detail.avatarUrl
-    let res = await WXAPI.default.uploadFile(wx.getStorageSync('token'), avatarUrl)
-    if (res.code != 0) {
-      wx.showToast({
-        title: res.msg,
-        icon: 'none'
-      })
-      return
-    }
-    res = await WXAPI.default.modifyUserInfo({
-      token: wx.getStorageSync('token'),
-      avatarUrl: res.data.url,
-    })
-    if (res.code != 0) {
-      wx.showToast({
-        title: res.msg,
-        icon: 'none'
-      })
-      return
-    }
-    wx.showToast({
-      title: '设置成功',
-    })
-    
   }
 })

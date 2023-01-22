9e332b9b9cab968126a780233e84e16faa6616d9
清 <claude-zq@qq.com>
2023-01-22 18:16:50 +0800

    feat: init blob srv

---
+++ server/cmd/auth/initialize/blob_service.go
@@ -0,0 +1,71 @@
+package initialize
+
+import (
+	"github.com/CyanAsterisk/FreeCar/server/cmd/profile/global"
+	"github.com/CyanAsterisk/FreeCar/server/cmd/profile/kitex_gen/blob/blobservice"
+	"github.com/CyanAsterisk/FreeCar/shared/consts"
+	"github.com/CyanAsterisk/FreeCar/shared/middleware"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/loadbalance"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/kitex-contrib/obs-opentelemetry/provider"
+	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	nacos "github.com/kitex-contrib/registry-nacos/resolver"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+)
+
+// InitBlob to init blob service
+func InitBlob() {
+	// init resolver
+	// Read configuration information from nacos
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: global.NacosConfig.Host,
+			Port:   global.NacosConfig.Port,
+		},
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         global.NacosConfig.Namespace,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              consts.NacosLogDir,
+		CacheDir:            consts.NacosCacheDir,
+		LogLevel:            consts.NacosLogLevel,
+	}
+
+	nacosCli, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		})
+	r := nacos.NewNacosResolver(nacosCli, nacos.WithGroup(consts.BlobGroup))
+	if err != nil {
+		hlog.Fatalf("new nacos client failed: %s", err.Error())
+	}
+	provider.NewOpenTelemetryProvider(
+		provider.WithServiceName(global.ServerConfig.BlobSrvInfo.Name),
+		provider.WithExportEndpoint(global.ServerConfig.OtelInfo.EndPoint),
+		provider.WithInsecure(),
+	)
+
+	// create a new client
+	c, err := blobservice.NewClient(
+		global.ServerConfig.BlobSrvInfo.Name,
+		client.WithResolver(r),                                     // service discovery
+		client.WithLoadBalancer(loadbalance.NewWeightedBalancer()), // load balance
+		client.WithMuxConnection(1),                                // multiplexing
+		client.WithMiddleware(middleware.CommonMiddleware),
+		client.WithInstanceMW(middleware.ClientMiddleware),
+		client.WithSuite(tracing.NewClientSuite()),
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: global.ServerConfig.BlobSrvInfo.Name}),
+	)
+	if err != nil {
+		klog.Fatalf("ERROR: cannot init client: %v\n", err)
+	}
+	global.BlobClient = c
+}

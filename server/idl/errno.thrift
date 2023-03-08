b94105ef257a26211d20ca5f1ac3ef713db057e4
清 <claude-zq@qq.com>
2023-03-08 10:15:38 +0800

    feat: update errno

--- server/idl/errno.thrift
+++ server/idl/errno.thrift
@@ -1,12 +1,21 @@
 namespace go errno
 
-enum Err
-{
-    Success               = 10000,
-    BadRequest            = 10001,
-	GenerateTokenFail     = 10002,
-	RequestServerFail     = 10003,
-	BindAndValidateFail   = 10004,
-	ParamErr              = 10005,
-	AuthorizeFail         = 10006,
+enum Err {
+    Success            = 0,
+    BadRequest         = 10000,
+    ParamsErr          = 10001,
+    AuthorizeFail      = 10002,
+    RPCAuthSrvErr      = 20000,
+    AuthSrvErr         = 20001,
+    RPCBlobSrvErr      = 30000,
+    BlobSrvErr         = 30001,
+    RPCCarSrvErr       = 40000,
+    CarSrvErr          = 40001,
+    RPCProfileSrvErr   = 50000,
+    ProfileSrvErr      = 50001,
+    RPCTripSrvErr      = 60000,
+    TripSrvErr         = 60001,
+    RecordNotFound     = 70000,
+    RecordAlreadyExist = 70001,
+    DirtyData          = 70003,
 }

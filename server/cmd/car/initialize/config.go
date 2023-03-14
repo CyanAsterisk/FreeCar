2edb107df7bea05c0b9aebabffcd0e4e7ab8ba4f
L2ncE <llance_24@foxmail.com>
2023-03-14 20:54:59 +0800

    feat: add consul as config server in car

---
+++ server/cmd/car/initialize/config.go
@@ -0,0 +1,52 @@
+package initialize
+
+import (
+	"net"
+	"strconv"
+
+	"github.com/CyanAsterisk/FreeCar/server/cmd/auth/config"
+	"github.com/CyanAsterisk/FreeCar/server/shared/consts"
+	"github.com/CyanAsterisk/FreeCar/server/shared/tools"
+	"github.com/bytedance/sonic"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/hashicorp/consul/api"
+	"github.com/spf13/viper"
+)
+
+// InitConfig to init consul config server
+func InitConfig() {
+	v := viper.New()
+	v.SetConfigFile(consts.CarConfigPath)
+	if err := v.ReadInConfig(); err != nil {
+		hlog.Fatalf("read viper config failed: %s", err.Error())
+	}
+	if err := v.Unmarshal(&config.GlobalConsulConfig); err != nil {
+		hlog.Fatalf("unmarshal err failed: %s", err.Error())
+	}
+	hlog.Infof("Config Info: %v", config.GlobalConsulConfig)
+
+	cfg := api.DefaultConfig()
+	cfg.Address = net.JoinHostPort(
+		config.GlobalConsulConfig.Host,
+		strconv.Itoa(config.GlobalConsulConfig.Port))
+	consulClient, err := api.NewClient(cfg)
+	if err != nil {
+		hlog.Fatalf("new consul client failed: %s", err.Error())
+	}
+	content, _, err := consulClient.KV().Get(config.GlobalConsulConfig.Key, nil)
+	if err != nil {
+		hlog.Fatalf("consul kv failed: %s", err.Error())
+	}
+
+	err = sonic.Unmarshal(content.Value, &config.GlobalServerConfig)
+	if err != nil {
+		hlog.Fatalf("sonic unmarshal config failed: %s", err.Error())
+	}
+
+	if config.GlobalServerConfig.Host == "" {
+		config.GlobalServerConfig.Host, err = tools.GetLocalIPv4Address()
+		if err != nil {
+			hlog.Fatalf("get localIpv4Addr failed:%s", err.Error())
+		}
+	}
+}

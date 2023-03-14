a71a7c0ebf1169b736055bf844ff98bec00aa1be
L2ncE <llance_24@foxmail.com>
2023-03-14 21:08:08 +0800

    feat: add consul config center for profile

--- server/cmd/profile/initialize/blob_service.go
+++ server/cmd/profile/initialize/blob_service.go
@@ -1,51 +1,28 @@
 package initialize
 
 import (
+	"fmt"
+
 	"github.com/CyanAsterisk/FreeCar/server/cmd/profile/config"
-	"github.com/CyanAsterisk/FreeCar/server/shared/consts"
 	"github.com/CyanAsterisk/FreeCar/server/shared/kitex_gen/blob/blobservice"
 	"github.com/CyanAsterisk/FreeCar/server/shared/middleware"
-	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/loadbalance"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/kitex-contrib/obs-opentelemetry/provider"
 	"github.com/kitex-contrib/obs-opentelemetry/tracing"
-	nacos "github.com/kitex-contrib/registry-nacos/resolver"
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/vo"
+	consul "github.com/kitex-contrib/registry-consul"
 )
 
 // InitBlob to init blob service
 func InitBlob() blobservice.Client {
 	// init resolver
-	// Read configuration information from nacos
-	sc := []constant.ServerConfig{
-		{
-			IpAddr: config.GlobalNacosConfig.Host,
-			Port:   config.GlobalNacosConfig.Port,
-		},
-	}
-
-	cc := constant.ClientConfig{
-		NamespaceId:         config.GlobalNacosConfig.Namespace,
-		TimeoutMs:           5000,
-		NotLoadCacheAtStart: true,
-		LogDir:              consts.NacosLogDir,
-		CacheDir:            consts.NacosCacheDir,
-		LogLevel:            consts.NacosLogLevel,
-	}
-
-	nacosCli, err := clients.NewNamingClient(
-		vo.NacosClientParam{
-			ClientConfig:  &cc,
-			ServerConfigs: sc,
-		})
-	r := nacos.NewNacosResolver(nacosCli, nacos.WithGroup(consts.BlobGroup))
+	r, err := consul.NewConsulResolver(fmt.Sprintf("%s:%d",
+		config.GlobalConsulConfig.Host,
+		config.GlobalConsulConfig.Port))
 	if err != nil {
-		hlog.Fatalf("new consul client failed: %s", err.Error())
+		klog.Fatalf("new consul client failed: %s", err.Error())
 	}
 	provider.NewOpenTelemetryProvider(
 		provider.WithServiceName(config.GlobalServerConfig.BlobSrvInfo.Name),

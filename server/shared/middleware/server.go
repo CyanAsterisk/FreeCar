fb9e1bb1754cd8d9c3890c8aa176c14c62d40054
Qing Zhou <claude-zq@qq.com>
2023-03-27 11:20:37 +0800

    fix: fix bugs (#113)
    * revert: revert AuthServiceImpl
    
    * fix: rename var
    
    * test: fix profile bug
    
    * chore: Comment out the rpc middleware
    
    * fix: init licenseManager
    
    * fix: fix bug
    
    * fix: fix bug

--- server/shared/middleware/server.go
+++ server/shared/middleware/server.go
@@ -4,8 +4,6 @@ import (
 	"context"
 
 	"github.com/cloudwego/kitex/pkg/endpoint"
-	"github.com/cloudwego/kitex/pkg/klog"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
 )
 
 var _ endpoint.Middleware = ServerMiddleware
@@ -13,9 +11,9 @@ var _ endpoint.Middleware = ServerMiddleware
 // ServerMiddleware server middleware print client address
 func ServerMiddleware(next endpoint.Endpoint) endpoint.Endpoint {
 	return func(ctx context.Context, req, resp interface{}) (err error) {
-		ri := rpcinfo.GetRPCInfo(ctx)
+		//ri := rpcinfo.GetRPCInfo(ctx)
 		// get client information
-		klog.Infof("[KITEX] client address: %v\n", ri.From().Address())
+		//klog.Infof("[KITEX] client address: %v\n", ri.From().Address())
 		if err = next(ctx, req, resp); err != nil {
 			return err
 		}

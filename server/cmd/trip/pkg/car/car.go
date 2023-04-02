a4c0fd61fad2d68eac7357da588630a87bdc5bad
清 <claude-zq@qq.com>
2023-04-02 17:32:35 +0800

    fix: fix car pkg

--- server/cmd/trip/pkg/car/car.go
+++ server/cmd/trip/pkg/car/car.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/CyanAsterisk/FreeCar/server/shared/errno"
 	"github.com/CyanAsterisk/FreeCar/server/shared/id"
 	"github.com/CyanAsterisk/FreeCar/server/shared/kitex_gen/base"
 	"github.com/CyanAsterisk/FreeCar/server/shared/kitex_gen/car"
 	"github.com/CyanAsterisk/FreeCar/server/shared/kitex_gen/car/carservice"
+	"github.com/CyanAsterisk/FreeCar/server/shared/tools"
 )
 
 // Manager defines a car manager.
@@ -22,18 +24,22 @@ func (m *Manager) Verify(c context.Context, cid id.CarID, aid id.AccountID) erro
 		AccountId: int64(aid),
 	})
 	if err != nil {
-		return fmt.Errorf("cannot get car: %v", err)
+		return errno.RPCCarSrvErr
+	}
+	if err = tools.ParseBaseResp(resp.BaseResp); err != nil {
+		return err
 	}
 	if resp.Car.Status != base.CarStatus_LOCKED {
-		return fmt.Errorf("cannot unlock;car status is %v", resp.Car.Status)
+		return errno.BadRequest.WithMessage(fmt.Sprintf("cannot unlock;car status is %v", resp.Car.Status))
 	}
 	return nil
 }
 
 // Unlock unlocks a car.
 func (m *Manager) Unlock(c context.Context, cid id.CarID, aid id.AccountID, tid id.TripID, avatarURL string) error {
-	_, err := m.CarService.UnlockCar(c, &car.UnlockCarRequest{
-		Id: cid.String(),
+	resp, err := m.CarService.UnlockCar(c, &car.UnlockCarRequest{
+		Id:        cid.String(),
+		AccountId: aid.Int64(),
 		Driver: &base.Driver{
 			Id:        aid.Int64(),
 			AvatarUrl: avatarURL,
@@ -41,19 +47,19 @@ func (m *Manager) Unlock(c context.Context, cid id.CarID, aid id.AccountID, tid 
 		TripId: tid.String(),
 	})
 	if err != nil {
-		return fmt.Errorf("cannot unlock: %v", err)
+		return errno.RPCCarSrvErr
 	}
-	return nil
+	return tools.ParseBaseResp(resp.BaseResp)
 }
 
 // Lock locks a car.
 func (m *Manager) Lock(c context.Context, cid id.CarID, aid id.AccountID) error {
-	_, err := m.CarService.LockCar(c, &car.LockCarRequest{
+	resp, err := m.CarService.LockCar(c, &car.LockCarRequest{
 		AccountId: int64(aid),
 		Id:        cid.String(),
 	})
 	if err != nil {
-		return fmt.Errorf("cannot lock: %v", err)
+		return errno.RPCCarSrvErr
 	}
-	return nil
+	return tools.ParseBaseResp(resp.BaseResp)
 }

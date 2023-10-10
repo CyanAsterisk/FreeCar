934e004c64ea6d1d2bcf8e621a1874ccf5db9c96
claude-zq <claude-zq@qq.com>
2023-10-10 14:11:06 +0800

    fix: fix wrong char

--- server/shared/middleware/cors.go
+++ server/shared/middleware/cors.go
@@ -14,7 +14,7 @@ func Cors() app.HandlerFunc {
 
 		c.Header("Access-Control-Allow-Origin", consts.CorsAddress)
 		c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT，DELETE")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
